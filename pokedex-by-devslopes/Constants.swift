043018efa23457db6caf40c7555cb860b2b50c3e
Holly Klose <hollyklose@gmail.com>
2016-02-27 18:04:29 -0500

    done with videos for this app

---
+++ pokedex-by-devslopes/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex-by-devslopes
+//
+//  Created by Holly Klose on 2/27/16.
+//  Copyright © 2016 Holly Klose. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
